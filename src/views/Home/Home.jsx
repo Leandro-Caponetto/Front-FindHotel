28bcd8e7577e4b93a4a38609da1f4ee87a19e8b2
daochoam <daochoam@unal.edu.co>
2023-08-15 16:41:24 -0500

    Configuration Frontend ReduxToolkit + others

---
+++ src/views/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import styles from './Home.module.css';
+
+function Home(props) {
+  return (
+    <div className={styles.Home}> Home Component </div>
+  )
+}
+
+export default Home;
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  assetsInclude: '*/.JPG',
+  optimizeDeps: {
+    allowNodeBuiltins: ['react', 'react-dom'],
+  }
 })
