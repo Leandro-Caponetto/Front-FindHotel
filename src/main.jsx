1affef65b093bc87ca1687e4d5e973d2fd20b90d
jorhedev <hijorhe@gmail.com>
2023-08-15 16:23:26 -0600

    errorRoutes

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Provider } from 'react-redux'
-import { store } from '@reduxjs/toolkit'
+// import { Provider } from 'react-redux'
+// import { store } from '@reduxjs/toolkit'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Provider store={store}>
+
+    <BrowserRouter>
       <App />
-    </Provider>
-  </React.StrictMode>,
+    </BrowserRouter>
+
+
 )
+
+
+    {/* <Provider store={store}> */}
