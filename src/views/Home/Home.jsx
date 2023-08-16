b343e0f539a8cd17b7453f032436f2a23f60d978
Karol Castilla Cruz <karolcastilla@hotmail.com>
2023-08-15 20:55:06 -0500

    TrendingDestinations

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import styles from './Home.module.css';
+import TrendDestinations from '../../components/trendDestinations';
 
 const Home = () => {
   return (
-    <div className={styles.Home}> Home Component </div>
+    <div>
+    <TrendDestinations/>
+    </div>
   );
 };
 
