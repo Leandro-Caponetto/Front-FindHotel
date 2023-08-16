0eb84786f8f41814970e28051ca8161fd0d98166
jorhedev <hijorhe@gmail.com>
2023-08-16 10:36:48 -0600

    fixes design home

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -2,20 +2,21 @@ import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import NavBar from '../../components/NavBar/NavBar'
 import styles from './Home.module.css';
-import TrendDestinations from '../../components/trendDestinations';
 import Footer from '../../components/Footer/Footer';
+import TrendDestinations from '../../components/TrendDestination/trendDestinations';
 
 const Home = () => {
   return (
 
-    <>
-      <NavBar />
-      <TrendDestinations />
-      <div className={styles.Footer}>
-        <Footer />
-      </div>
 
-    </>
+    <div className={styles.container}>
+     <NavBar/>
+     <TrendDestinations/>
+     <TrendDestinations/>
+     <Footer />
+    </div>
+
+
   )
 }
 
