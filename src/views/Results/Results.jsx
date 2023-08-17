a6f7878d0ae06c7e1abec7522010fff5739a33a4
Karol Castilla Cruz <karolcastilla@hotmail.com>
2023-08-17 12:27:29 -0500

    HotelList has been created

---
+++ src/views/Results/Results.jsx
@@ -0,0 +1,18 @@
+import React from 'react'; 
+import HotelList from '../../components/HotelList/HotelList'
+import NavBar from '../../components/NavBar/NavBar'
+
+
+const Results = () => {
+  return (
+
+    <div>
+      <NavBar/>
+     <HotelList/>
+    </div>
+
+
+  )
+}
+
+export default Results;
