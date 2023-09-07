2a7aeedbc7dfd7fce46f8d7a54563751c06a2208
daochoam <daochoam@unal.edu.co>
2023-09-07 12:35:20 -0500

    Hotel

--- src/views/DashboardHotel/DashboardHotel.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import Dashboard from '../../components/Dashboard/Hotel/DashboarHotel'
-
-function HotelDashboard() {
-  return (
-    <Dashboard />
-  )
-}
-export default HotelDashboard
